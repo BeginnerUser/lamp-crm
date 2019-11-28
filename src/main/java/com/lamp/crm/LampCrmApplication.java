0983b55233afce54b6eec39a05d696a297e0eb08
孟如青 <m15225437987@163.com>
2019-11-28 18:30:31 +0800

    客户管理项目

---
+++ src/main/java/com/lamp/crm/LampCrmApplication.java
@@ -0,0 +1,13 @@
+package com.lamp.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LampCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamp/crm/LampCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamp.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LampCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
